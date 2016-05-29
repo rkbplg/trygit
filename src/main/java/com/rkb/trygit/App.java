1bff4ced2615f87c1872d4dd2c17c2b8442be70c
rkbplg <ravi.batra@gmail.com>
2016-05-29 22:33:14 +1000

    first commit

---
+++ src/main/java/com/rkb/trygit/App.java
@@ -0,0 +1,13 @@
+package com.rkb.trygit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
